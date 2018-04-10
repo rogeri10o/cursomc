8a54d28c302a714783fb4a581ef1f6a8209bbce2
rogeri10o <rogeri10o@yahoo.com.br>
2018-04-09 22:50:21 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/rogreis/cursomc/domain/Pagamento.java
+++ src/main/java/com/rogreis/cursomc/domain/Pagamento.java
@@ -14,27 +14,36 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.rogreis.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy=InheritanceType.JOINED)
 public abstract class Pagamento implements Serializable {
-	private static final long serialVersionUID = -5184159872586049817L;
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	private Integer id;
 	private Integer estado;
+
 	@JsonIgnore
-	@JoinColumn(name = "pedido_id")
 	@OneToOne
+	@JoinColumn(name="pedido_id")
 	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
-
 	}
 
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
 		super();
 		this.id = id;
-		this.estado = estado.getCod();
+		this.estado = (estado==null) ? null : estado.getCod();
 		this.pedido = pedido;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public EstadoPagamento getEstado() {
@@ -43,6 +52,14 @@ public abstract class Pagamento implements Serializable {
 
 	public void setEstado(EstadoPagamento estado) {
 		this.estado = estado.getCod();
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
 	}
 
 	@Override
@@ -69,6 +86,7 @@ public abstract class Pagamento implements Serializable {
 			return false;
 		return true;
 	}
+	
 	
 	
 }
